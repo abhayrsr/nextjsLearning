198dd6760bf06a1590c7364905c1036af40f2e97
abhay <abhayrajsinghrana123@gmail.com>
2025-04-08 00:25:27 +0530

    metadata

--- routes/src/app/product/[productId]/page.tsx
+++ routes/src/app/product/[productId]/page.tsx
@@ -1,5 +1,22 @@
 import { notFound } from "next/navigation";
-export default async function ProductDetails({params}  : {params: Promise<{ productId: string}>; }) {
+import {Metadata} from "next";
+
+type Props = {
+    params: Promise <{productId: string}>;
+}
+
+export const generateMetadata = async ({params}: Props): Promise<Metadata> => {
+    const id = (await params).productId;
+    const title = await new Promise((resolve) => {
+        setTimeout(() => {
+            resolve(`iphone ${id}`);
+        }, 100);
+    });
+    return{
+        title: `Product ${title}`
+    }
+}
+export default async function ProductDetails({params}  : Props) {
     const productId = (await params).productId
     if(parseInt(productId) > 1000){
         notFound();
